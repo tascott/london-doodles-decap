7cbbac7fbe8a33b4b8043c1cf739143a1d919042
TomScott <contact@tascott.co.uk>
2025-01-03 21:10:40 +0000

    New settings for next 15

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
     webpack: (cfg) => {
-        cfg.module.rules.push(
-            {
-                test: /\.md$/,
-                loader: 'frontmatter-markdown-loader',
-                options: {mode: ['react-component']}
-            }
-        )
-        return cfg
-    }
-}
+        cfg.module.rules.push({
+            test: /\.md$/,
+            loader: 'frontmatter-markdown-loader',
+            options: {mode: ['react-component']},
+        });
+        return cfg;
+    },
+};
+
+module.exports = nextConfig;
